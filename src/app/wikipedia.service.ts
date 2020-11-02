d5f9c49f8afd5b36eb418b203d274bf8a68e7ed7
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-02 16:28:13 -0300

    Really Weird Behaviour

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 
+const WIKIPEDIA_URL = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=';
+
 @Injectable({
   providedIn: 'root',
 })
 export class WikipediaService {
-  constructor() {}
-
   search(term: string): string {
     return `I am Wikipedia search results. Search term: ${term}`;
   }
